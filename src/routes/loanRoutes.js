0a1456d11049638007b7b99b37dc36ee2e0b2e76
Rizkiarahman000 <rizkiarahman@students.undip.ac.id>
2026-04-28 08:57:32 +0700

    Update loanRoutes.js

--- src/routes/loanRoutes.js
+++ src/routes/loanRoutes.js
@@ -4,6 +4,8 @@ import { LoanController } from '../controllers/loanController.js';
 const router = express.Router();
 
 router.get('/', LoanController.getLoans);
+router.get('/top-borrowers', LoanController.getTopBorrowers); // TAMBAH INI
 router.post('/', LoanController.createLoan);
+router.put('/:id/return', LoanController.returnBook);
 
 export default router;
