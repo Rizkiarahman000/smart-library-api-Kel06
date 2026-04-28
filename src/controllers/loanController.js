f6b599310583efb0cdde58daa7e5e81bb8f461a8
Rizkiarahman000 <rizkiarahman@students.undip.ac.id>
2026-04-28 08:56:32 +0700

    Update loanController.js

--- src/controllers/loanController.js
+++ src/controllers/loanController.js
@@ -3,14 +3,14 @@ import { LoanModel } from '../models/loanModel.js';
 export const LoanController = {
   async createLoan(req, res) {
     const { book_id, member_id, due_date } = req.body;
+
     try {
       const loan = await LoanModel.createLoan(book_id, member_id, due_date);
       res.status(201).json({
-        message: "Peminjaman berhasil dicatat!",
+        message: 'Peminjaman berhasil dicatat!',
         data: loan
       });
     } catch (err) {
-      // Jika stok habis atau ID salah, kirim status 400 (Bad Request)
       res.status(400).json({ error: err.message });
     }
   },
@@ -19,6 +19,32 @@ export const LoanController = {
     try {
       const loans = await LoanModel.getAllLoans();
       res.json(loans);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  },
+
+  async returnBook(req, res) {
+    try {
+      const { id } = req.params;
+      const result = await LoanModel.returnBook(id);
+
+      res.json({
+        message: 'Buku berhasil dikembalikan.',
+        data: result
+      });
+    } catch (err) {
+      res.status(400).json({ error: err.message });
+    }
+  },
+
+  async getTopBorrowers(req, res) {
+    try {
+      const topBorrowers = await LoanModel.getTopBorrowers();
+      res.json({
+        message: 'Top 3 peminjam buku berhasil diambil',
+        data: topBorrowers
+      });
     } catch (err) {
       res.status(500).json({ error: err.message });
     }
